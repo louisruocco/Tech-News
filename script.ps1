f8ae92f4a89e11700a8e1c39a92121f7f52172f0
Louis Ruocco <louisruocco1@gmail.com>
2023-06-04 22:50:58 +0100

    hid api key

--- script.ps1
+++ script.ps1
@@ -1,4 +1,6 @@
-$req = Invoke-RestMethod 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCFtEEv80fQVKkD4h1PF-Xqw&maxResults=1&order=date&type=video&key=AIzaSyBD7heoYLk2epcRlDzOUONLYhCpL8INtRM'
+$key = Get-Content ./info.txt
+
+$req = Invoke-RestMethod "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCFtEEv80fQVKkD4h1PF-Xqw&maxResults=1&order=date&type=video&key=$key"
 
 $date = Get-Date -Format "yyyy-MM-dd"
 
