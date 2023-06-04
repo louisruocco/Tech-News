73fbf328e7e29091294a285a81d5fdb94c22fb20
Louis Ruocco <louisruocco1@gmail.com>
2023-06-04 22:35:21 +0100

    able to compare date of vid with today's date, working on logic for it

--- script.ps1
+++ script.ps1
@@ -1,7 +1,13 @@
-$date = Get-Date
-
 $req = Invoke-RestMethod 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCFtEEv80fQVKkD4h1PF-Xqw&maxResults=1&order=date&type=video&key=AIzaSyBD7heoYLk2epcRlDzOUONLYhCpL8INtRM'
-# $req.items.snippet
+
+$date = Get-Date -Format "yyyy-MM-ddThh:mm:ssZ"
+$date
+
+$vidDate = $req.items.snippet.publishTime
+$vidDate
+
 $videoUrl = $req.items.id.videoId
 
-# start msedge https://www.youtube.com/watch?v=$videoUrl
+if($date -ge $vidDate){
+    Start-Process msedge https://www.youtube.com/watch?v=$videoUrl
+}
