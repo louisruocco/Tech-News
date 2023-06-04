0fbee87631e89db0f07f3bfa45b56b62779a64b1
Louis Ruocco <louisruocco1@gmail.com>
2023-06-04 22:43:58 +0100

    Microsoft channel done

--- script.ps1
+++ script.ps1
@@ -1,13 +1,11 @@
 $req = Invoke-RestMethod 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCFtEEv80fQVKkD4h1PF-Xqw&maxResults=1&order=date&type=video&key=AIzaSyBD7heoYLk2epcRlDzOUONLYhCpL8INtRM'
 
-$date = Get-Date -Format "yyyy-MM-ddThh:mm:ssZ"
-$date
+$date = Get-Date -Format "yyyy-MM-dd"
 
-$vidDate = $req.items.snippet.publishTime
-$vidDate
+$vidDate = $req.items.snippet.publishTime -split 'T' -replace $vidDate[1], ''
 
 $videoUrl = $req.items.id.videoId
 
-if($date -ge $vidDate){
+if($date -eq $vidDate){
     Start-Process msedge https://www.youtube.com/watch?v=$videoUrl
 }
