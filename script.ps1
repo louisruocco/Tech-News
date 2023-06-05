5ddee469567676c86d2170bc473685c07d7c5904
Louis Ruocco <louisruocco1@gmail.com>
2023-06-05 08:48:22 +0100

    changed youtube api date string format, script should now work as intended

--- script.ps1
+++ script.ps1
@@ -4,7 +4,8 @@ $req = Invoke-RestMethod "https://youtube.googleapis.com/youtube/v3/search?part=
 
 $date = Get-Date -Format "yyyy-MM-dd"
 
-$vidDate = $req.items.snippet.publishTime -split 'T' -replace $vidDate[1], ''
+$vidDate = $req.items.snippet.publishTime -replace "T.*"
+$vidDate
 
 $videoUrl = $req.items.id.videoId
 
