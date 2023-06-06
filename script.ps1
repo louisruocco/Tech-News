03c173040077e7a2b275ac18578bfa69a5e4aa72
Louis Ruocco <louisruocco1@gmail.com>
2023-06-06 08:03:12 +0100

    added yesterday's date to avoid gaps

--- script.ps1
+++ script.ps1
@@ -2,13 +2,16 @@ $key = Get-Content ./info.txt
 
 $req = Invoke-RestMethod "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCFtEEv80fQVKkD4h1PF-Xqw&maxResults=1&order=date&type=video&key=$key"
 
-$date = Get-Date -Format "yyyy-MM-dd"
+$today = Get-Date -Format "yyyy-MM-dd"
+$yesterday = (Get-Date).AddDays(-1).ToString('yyyy-MM-dd')
+$yesterday
+
 
 $vidDate = $req.items.snippet.publishTime -replace "T.*"
 $vidDate
 
 $videoUrl = $req.items.id.videoId
 
-if($date -eq $vidDate){
+if($vidDate -eq $today -or $vidDate -eq $yesterday ){
     Start-Process msedge https://www.youtube.com/watch?v=$videoUrl
 }
