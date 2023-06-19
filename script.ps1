52292b3e8aaa41e57e1eff7dff3d9ba702695592
Louis Ruocco <louisruocco1@gmail.com>
2023-06-19 08:29:40 +0100

    re-added yesterday variable as videos were being missed

--- script.ps1
+++ script.ps1
@@ -3,12 +3,13 @@ $key = Get-Content "E:\Code\Tech News\info.txt"
 $req = Invoke-RestMethod "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC0m-80FnNY2Qb7obvTL_2fA&channelType=any&maxResults=1&order=date&key=$key"
 
 $today = Get-Date -Format "yyyy-MM-dd"
+$yesterday = (Get-Date).AddDays(-1).ToString("yyyy-MM-dd")
 
 $vidDate = $req.items.snippet.publishTime -replace "T.*"
 
 $videoUrl = $req.items.id.videoId
 
-if($vidDate -eq $today){
+if($vidDate -eq $today -or $vidDate -eq $yesterday){
     Start-Sleep 2
     Start-Process msedge https://www.youtube.com/watch?v=$videoUrl
 }
