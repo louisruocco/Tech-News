b07ff8089cbfc0ac7eb47bed753f023b2482096c
Louis Ruocco <louisruocco1@gmail.com>
2023-06-21 06:27:08 +0100

    added made by google channel

--- script.ps1
+++ script.ps1
@@ -1,15 +1,22 @@
 $key = Get-Content "E:\Code\Tech News\info.txt"
 
-$req = Invoke-RestMethod "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC0m-80FnNY2Qb7obvTL_2fA&channelType=any&maxResults=1&order=date&key=$key"
+$azure = Invoke-RestMethod "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC0m-80FnNY2Qb7obvTL_2fA&channelType=any&maxResults=1&order=date&key=$key"
+$pixel = Invoke-RestMethod "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCIG1k8umaCIIrujZPzZPIMA&maxResults=1&order=date&key=$key"
 
 $today = Get-Date -Format "yyyy-MM-dd"
 $yesterday = (Get-Date).AddDays(-1).ToString("yyyy-MM-dd")
 
-$vidDate = $req.items.snippet.publishTime -replace "T.*"
+$AzureVidDate = $azure.items.snippet.publishTime -replace "T.*"
+$AzurevideoUrl = $azure.items.id.videoId
+$PixelVidDate = $pixel.items.snippet.publishTime -replace "T.*"
+$PixelVideoUrl = $pixel.items.id.videoId
 
-$videoUrl = $req.items.id.videoId
-
-if($vidDate -eq $today -or $vidDate -eq $yesterday){
+if($AzureVidDate -eq $today -or $AzureVidDate -eq $yesterday){
     Start-Sleep 2
-    Start-Process msedge https://www.youtube.com/watch?v=$videoUrl
+    Start-Process msedge https://www.youtube.com/watch?v=$AzurevideoUrl
+}
+
+if($PixelVidDate -eq $today -or $PixelVidDate -eq $yesterday){
+    Start-Sleep 2
+    Start-Process msedge "--new-tab https://www.youtube.com/watch?v=$PixelVideoUrl"
 }
